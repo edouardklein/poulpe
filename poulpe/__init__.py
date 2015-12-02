709d35e7b9df637b0fe3d4221b8e151b7d66a614
Edouard Klein <edouardklein@gmail.com>
2015-12-02 14:54:10 +0100

    WIP on the install process

--- poulpe/__init__.py
+++ poulpe/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+import subprocess
 
 __author__ = 'Edouard Klein'
 __email__ = 'edou -at- rdklein.fr'
 __version__ = '0.0.0'
+
+
+def cmd(c):
+    '''Run the command in a shell'''
+    return subprocess.check_output(c, shell=True).decode('utf8')
--- poulpe/poulpe.py
+++ poulpe/poulpe.py
@@ -3,10 +3,29 @@
 poulpe init
 """
 from docopt import docopt
-import poulpe
+import os
+import logging
+from poulpe import cmd
+
+logging.basicConfig(level=logging.INFO)
+
+
+def init():
+    '''Install the Poulpe's hooks and scripts.'''
+    src_dir = os.path.dirname(os.path.realpath(__file__))
+    dst_dir = os.getcwd()
+    logging.info('Creating a new case')
+    cmd('git init')
+    cmd('git remote add origin '+src_dir)
+    cmd('git fetch')
+    cmd('git checkout -t origin/master')
+    cmd('ln -s '+src_dir+'/poulpe/post-commit ' + dst_dir +
+        '/.git/hooks/')
+    cmd('mkdir '+dst_dir+'/.git/artefacts')
+    cmd('ln -s '+src_dir+'/src/artefacts/* '+dst_dir+'/.git/artefacts/')
+
 
 def main():
     arguments = docopt(__doc__)
     if arguments['init']:
-        print("INIT")
-    print("END")
+        init()
